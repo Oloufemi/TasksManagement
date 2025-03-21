4315f778a89c524b6067e9ec501dcaeda5ce1792
Oloufemi Adekambi <adekambiromaric@gmail.com>
2025-03-21 22:48:49 +0100

    feat(front):delete some files from code coverage

--- Front/jest.config.js
+++ Front/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  coverageDirectory: 'coverage/jest',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/app/works/works.module.ts'],
+  coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: [
-    '<rootDir>/node_modules/',
+    '<rootDir>/node_modules/','<rootDir>/src/app/works/works.module.ts'
   ],
 };

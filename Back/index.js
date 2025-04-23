4976c72f671dfe940c0a70ad9978e7cad13f088d
Oloufemi Adekambi <adekambiromaric@gmail.com>
2025-04-23 10:01:35 +0200

    feat(back): update gitignore back

--- Back/index.js
+++ Back/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const dotenv = require('dotenv');
-dotenv.config({ path: 'config.env' });
+dotenv.config({ path: '.env' });
 
 const app = require('./app');
 const connectDB = require('./database/connection');
